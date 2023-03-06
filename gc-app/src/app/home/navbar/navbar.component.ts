1b1003c42719d2f56e2231a7213ec7b6ddc08739
Mateusz Tabor <mtabor99@tlen.pl>
2023-03-06 15:37:04 +0100

    Implement authors section

--- gc-app/src/app/home/navbar/navbar.component.ts
+++ gc-app/src/app/home/navbar/navbar.component.ts
@@ -1,3 +1,4 @@
+import { ViewportScroller } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,4 +6,10 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  constructor(private viewportScroller: ViewportScroller) {}
+
+  scroll(id: string) {
+    this.viewportScroller.scrollToAnchor(id);
+  }
+}
